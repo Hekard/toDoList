0f174019a7b180b83e142663c5374dd7a0582b23
Hekard <lukasz.kleszczewski531@gmail.com>
2021-09-11 08:33:57 +0200

    add option to add and remove todo objects

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -21,8 +21,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 600px;
+  margin: 0 auto;
+  text-align: center;
+
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="container">
+      <input type="text" class="todo-input" placeholder="Item name" v-model="newTodo" @keyup.enter="addTodo">
+      <button type="button" v-on:click="addTodo">Add task</button> 
+    </div>
+    <div v-for="(todo,index) in todos" :key="todo.id" class="toDoItem">
+      <div class="toDo">
+        <div>{{todo.title}}</div>
+        <input type="text" v-model="todo.title">
+      </div>
+      <div class="remove-item" @click="removeToDo(index)">
+        &times;
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      newTodo: '',
+      idToDo: 3,
+      todos:[
+        {
+          'id': 1,
+          'title': 'Buy bread',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Be responsible',
+          'completed': false,
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo(){
+      if(this.newTodo.trim()==0){
+        return
+      }
+
+      this.todos.push({
+        id: this.idToDo,
+        title: this.newTodo,
+        completed: false,
+      })
+
+      this.newTodo = ''
+      this.this.idToDo++
+    },
+    removeToDo(index){
+      this.todos.splice(index, 1)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.todo-input { 
+  width: 80%; 
+  padding: 10px;
+}
+
+.toDoItem {
+  margin: 5px 0;
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 5px;
+}
+
+.remove-item {
+  cursor: pointer;
+}
+
+button {
+   background: green;
+   padding: 10px;
+   color: white;
+   margin-left: 15px;
+}
+
+
 h3 {
   margin: 40px 0 0;
 }
